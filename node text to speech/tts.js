ebc40194259a5a47c0454d29467a1313cfefe4f0
Vaibhav <vaibhavnandwani@gmail.com>
2020-05-06 03:50:37 +0530

    tts

---
+++ node text to speech/tts.js
@@ -0,0 +1,89 @@
+// To install dependencies, run: npm install
+const xmlbuilder = require('xmlbuilder');
+// request-promise has a dependency on request
+const rp = require('request-promise');
+const fs = require('fs');
+const readline = require('readline-sync');
+
+
+// Gets an access token.
+function getAccessToken(subscriptionKey) {
+    let options = {
+        method: 'POST',
+        uri: 'https://eastus.api.cognitive.microsoft.com/sts/v1.0/issuetoken',
+        headers: {
+            'Ocp-Apim-Subscription-Key': subscriptionKey
+        }
+    }
+    return rp(options);
+}
+
+// Converts text to speech using the input from readline.
+function textToSpeech(accessToken, text) {
+    // Create the SSML request.
+    var male=["en-GB-George-Apollo","en-IE-Sean","en-IN-Ravi-Apollo","en-US-BenjaminRUS","en-US-Guy24kRUS"]
+    var female=["en-AU-Catherine","en-AU-HayleyRUS","en-CA-Linda","en-GB-Susan-Apollo","en-IN-Heera-Apollo","en-IN-PriyaRUS","en-US-AriaRUS"];
+    
+    var voice=female[Math.floor(Math.random() * 7)];
+    
+    
+    let xml_body = xmlbuilder.create('speak')
+        .att('version', '1.0')
+        .att('xml:lang', 'en-us')
+        .ele('voice')
+        .att('xml:lang', 'en-us')
+        .att('name', voice) // Short name for 'Microsoft Server Speech Text to Speech Voice (en-US, Guy24KRUS)'
+        .txt(text)
+        .end();
+    // Convert the XML into a string to send in the TTS request.
+    let body = xml_body.toString();
+
+    let options = {
+        method: 'POST',
+        baseUrl: 'https://eastus.tts.speech.microsoft.com/',
+        url: 'cognitiveservices/v1',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken,
+            'cache-control': 'no-cache',
+            'User-Agent': 'modiji',
+            'X-Microsoft-OutputFormat': 'riff-24khz-16bit-mono-pcm',
+            'Content-Type': 'application/ssml+xml'
+        },
+        body: body
+    }
+
+    let request = rp(options)
+        .on('response', (response) => {
+            if (response.statusCode === 200) {
+                request.pipe(fs.createWriteStream('TTSOutput.wav'));
+                console.log('\nYour file is ready.\n')
+            }
+        });
+    return request;
+
+};
+
+// Use async and await to get the token before attempting
+// to convert text to speech.
+async function main() {
+    // Reads subscription key from env variable.
+    // You can replace this with a string containing your subscription key. If
+    // you prefer not to read from an env variable.
+    // e.g. const subscriptionKey = "your_key_here";
+    const subscriptionKey = "f8b1357f19db44b19ec88ef5961b5695";
+    if (!subscriptionKey) {
+        throw new Error('Environment variable for your subscription key is not set.')
+    };
+    // Prompts the user to input text.
+    const text = readline.question('What would you like to convert to speech? ');
+
+    try {
+        const accessToken = await getAccessToken(subscriptionKey);
+        await textToSpeech(accessToken, text);
+    } catch (err) {
+        console.log(`Something went wrong: ${err}`);
+    }
+}
+
+// Run the application
+main()
